fd08eb6a4f906c54cdd27859cf2e30773de86e49
Sourav Shit <90740835+rockAlfpha@users.noreply.github.com>
2024-02-25 18:33:18 +0530

    Add files via upload
    "Working on a new project"

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const articleRouter = require('./routes/articles')
+const app = express();
+
+app.set('view engine', 'ejs');
+app.get('/', (req, res)=>{
+    res.render('index')
+})
+
+app.use('/articles', articleRouter);
+
+app.listen(3000);
+
+const App(){
+    console.log("Hey there");
+}
